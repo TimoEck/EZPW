439f26f4f1750518f83c5be9bddbe03bc8bcbe79
Timo Eck <timo.eck1996@gmail.com>
2024-05-19 15:25:11 +0200

    Updated Class PasswordGenerator with a loop to close the application and choose for another password loop

--- pwgen.py
+++ pwgen.py
@@ -13,32 +13,37 @@ class PasswordGenerator:
 
     #Input set Option which character-set will be used
     def set_Options(self):
-       
-        selection = input("""
-╔══════════════════════════════════════════════════╗
-║ Choose Security-Level (Default is [B]):          ║
-║                                                  ║
-║ [A] Lowercases                                   ║
-║ [B] Lower- & Uppercase                           ║
-║ [C] Lower- & Uppercase with digits               ║
-║ [D] Top-Secret                                   ║
-║                                                  ║
-║                                 made by Timo Eck ║
-╚══════════════════════════════════════════════════╝
-""").upper()
-        
-        
-        if selection == "" or selection == "B":
-                self.character_set = string.ascii_lowercase
-        elif selection == "B":
-                self.character_set = string.ascii_lowercase +  string.ascii_uppercase
-        elif selection == "C":
-                self.character_set = string.ascii_lowercase + string.ascii_uppercase + string.digits     
-        elif selection == "D":
-                self.character_set = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-        else:
-            print("Please make a valid selection.")
-            return self.set_Options()
+        while True:
+           
+            selection = input("""
+    ╔══════════════════════════════════════════════════╗
+    ║ Choose Security-Level (Default is [B]):          ║
+    ║                                                  ║
+    ║ [A] Lowercases                                   ║
+    ║ [B] Lower- & Uppercase                           ║
+    ║ [C] Lower- & Uppercase with digits               ║
+    ║ [D] Top-Secret                                   ║
+    ║ [E] Close application                            ║
+    ║                                                  ║
+    ║                                 made by Timo Eck ║
+    ╚══════════════════════════════════════════════════╝
+    """).upper()
+            
+            
+            if selection == "" or selection == "B":
+                    self.character_set = string.ascii_lowercase +  string.ascii_uppercase
+            elif selection == "A":
+                    self.character_set = string.ascii_lowercase
+            elif selection == "C":
+                    self.character_set = string.ascii_lowercase + string.ascii_uppercase + string.digits     
+            elif selection == "D":
+                    self.character_set = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+            elif selection == "E":
+                    exit() 
+            else:
+                print("Please make a valid selection.")
+                continue
+            break
 
     # Input set length how many characters will be used
     def set_Length(self):
@@ -56,10 +61,20 @@ class PasswordGenerator:
         password = "".join(random.choice(self.character_set) for _ in range(self.length))
         return password
 
-PwGen = PasswordGenerator()
-option = PwGen.set_Options()
-length = PwGen.set_Length()
-Password = PwGen.generatePassword()
-print(f"Generated Password: {Password}")
+
+
+
+if __name__ == "__main__":
+    while True:
+        pw_gen = PasswordGenerator()
+        pw_gen.set_Options()
+        pw_gen.set_Length()
+        password = pw_gen.generatePassword()
+        if password:
+            print(f"Generated password: {password}")
+
+        another = input("Generate another password? (Y/N): ").upper()
+        if another != 'Y':
+            break
 
 
